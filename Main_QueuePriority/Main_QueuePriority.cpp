0d6364a93687db229627023017a0ca82614bbfd8
Exori <exori@mail.com>
2023-08-10 21:04:19 +0300

    Corrections to Homework Assignment on Priority Queue

--- Main_QueuePriority/Main_QueuePriority.cpp
+++ Main_QueuePriority/Main_QueuePriority.cpp
@@ -8,19 +8,19 @@ int main()
     system("chcp 1251>nul");
     QueuePriority a(10);
    
-    a.Add(10, 3);
-    a.Add(9, 2);
-    a.Add(8, 1);
-    a.Add(7, 3);
-    a.Add(6, 2);
-    a.Add(5, 1);
-    a.Add(4, 3);
-    a.Add(3, 2);
-    a.Add(2, 1);
+    a.Add(10, 4);
+    a.Add(5, 3);
+    a.Add(5, 5);
+    a.Add(8, 2);
     a.Add(1, 1);
-    cout << a<<endl;
-  
-    a.Extract();
+    
+    /*cout << a.Extract() << endl;
+    cout << a.Extract() << endl;
+    cout << a.Extract() << endl;
+    cout << a.Extract() << endl;*/
+    cout << a;
+
+   /* a.Extract();
     a.Extract();
     a.Extract();
     a.Extract();
@@ -31,7 +31,7 @@ int main()
     b = move(a);
     cout << b<<endl;
     QueuePriority c(move(b));
-    cout << c << endl;
+    cout << c << endl;*/
     system("pause");
 
 }
